4ece952c8dc10b9c0885b9c50dadbdaa63680383
Adryanne <126941916+adryannegorden@users.noreply.github.com>
2024-03-24 01:32:00 -0400

    added login button

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,14 +4,17 @@ function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
+    const handleLoginEnter = (event) => {
+        console.log("Entered Username:", username);
+        console.log("Entered Password:", password);
+    }
+
     const handleUsernameChange = (event) => {
         setUsername(event.target.value);
-        console.log('Username:', event.target.value);
     }
 
     const handlePasswordChange = (event) => {
         setPassword(event.target.value);
-        console.log('Password:', event.target.value);
     }
 
     return( 
@@ -20,12 +23,17 @@ function Login() {
                 <h1>Login</h1>
             </div>
             <div className="flex justify-center">
-                <label htmlFor="username">Username</label>
+                <label htmlFor="username"></label>
                 <input type="text" id="username" placeholder="Username" name="uname" value={username} onChange={handleUsernameChange} required className="border border-blue-500 rounded-md p-1"/>
             </div>
             <div className="flex justify-center">
-                <label htmlFor="password">Password</label>
+                <label htmlFor="password"></label>
                 <input type="password" id="password" placeholder="Password" name="pass" value={password} onChange={handlePasswordChange} required className="border border-blue-500 rounded-md p-1"/>
+            </div>
+            <div className="flex justify-center">
+                <button onClick={handleLoginEnter} className="border border-blue-500 rounded-md p-2">
+                    Enter
+                </button>
             </div>
         </>
     );
