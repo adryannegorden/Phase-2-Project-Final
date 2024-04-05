87bbd4b3b2d97047e1fa9d3bf7ceab489e8e19b7
Adryanne <126941916+adryannegorden@users.noreply.github.com>
2024-04-05 16:23:40 -0400

    added databases for each animal

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,10 @@ function Home() {
     return (
         <div>
             <h1 className="text-center font-bold">Welcome to the Animal facts!</h1>
+            <p className="text-center text-red-500 font-extrabold">
+                ADMIN NOTE: 
+                I will not be adding any other animals. I only care about Kiwis, Isopods, and Otters. Stop asking!
+            </p>
         </div>
     );
 }
